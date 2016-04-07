cc2d3947c217c537c4782b40e71f0cb8542fed7f
Henry Barton <hbarton2@sheffield.ac.uk>
2016-04-07 18:00:08 +0100

    Updated to correct reference sequencing naming to identify chromosomes

--- chromosomal_lastz.py
+++ chromosomal_lastz.py
@@ -7,24 +7,26 @@ import os
 # arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-ref', help='Whole genome fasta of reference species', required=True)
+parser.add_argument('-ref_name', help='Name of ref spp for file naming', required=True)
 parser.add_argument('-fa_list', help='List of chromosomal fastas to align to ref', required=True)
 parser.add_argument('-out', help='Output directory', required=True)
 args = parser.parse_args()
 
 # variables
 query_genomes = [fa.rstrip('\n').split() for fa in open(args.fa_list)]
-ref_genome = args.ref.split()
+ref_genome = args.ref
+ref_name = args.ref_name
 out_dir = args.out
 if not os.path.isdir(out_dir):
     os.mkdir(out_dir)
 
 # iterate through query genomes
 for genome in query_genomes:
-    output_prefix = out_dir + ref_genome[1] + '_vs_' + genome[1]
+    output_prefix = out_dir + ref_name + '_vs_' + genome[1]
 
     # construct commandline
     lastz_cmd = ('"lastz_latest ' +
-                 ref_genome[0] + '[multiple][nickname=' + ref_genome[1] + '] ' +
+                 ref_genome + '[multiple][nameparse=tag:\'>\'] ' +
                  genome[0] + '[nickname=' + genome[1] + '] '
                  '--gfextend '
                  '--chain '
