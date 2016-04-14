3d5bfc7e0ad1efe268cba486479423e250bda202
Henry Barton <hbarton2@sheffield.ac.uk>
2016-04-14 12:16:00 +0100

    Changed mergeGG.py for mergeZF.py

--- mergeZF.py
+++ mergeZF.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 # arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-fa_list', help='List file of fasta files to merge', required=True)
@@ -8,7 +9,7 @@ args = parser.parse_args()
 # variables
 fastas = [fa.rstrip('\n') for fa in open(args.fa_list)]
 output = open('/fastdata/bop15hjb/GT_data/Multispecies_alignment/Whole_genomes_chr_only/'
-              'Gallus_gallus.Galgal4.dna_sm.fa', 'w')
+              'Taeniopygia_guttata.taeGut3.2.4.dna_sm.fa', 'w')
 
 # merge
 for fa in fastas:
