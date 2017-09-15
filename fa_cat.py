c2cc35470855de0935d586bd94471802cf830212
henryjuho <juhob@hotmail.co.uk>
2017-09-15 14:52:29 +0100

    concatonates fa chromo parts

---
+++ fa_cat.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+
+
+def fasta_print(contig, seq):
+
+    print('>{}'.format(contig))
+
+    for i in range(0, len(seq), 60):
+        print(seq[i:i+60])
+
+
+def main():
+
+    sequence = ''
+    chromo = ''
+    prev_chromo = ''
+
+    for file_name in sys.stdin:
+        for line in open(file_name):
+            if line.startswith('>'):
+                prev_chromo = chromo
+                chromo = line.split(':')[0].replace('.', '')
+                if prev_chromo == chromo or prev_chromo == '':
+                    continue
+                else:
+                    fasta_print(prev_chromo, sequence)
+                    sequence = ''
+            else:
+                sequence += line.rstrip()
+
+    fasta_print(prev_chromo, sequence)
+
+if __name__ == '__main__':
+    main()
