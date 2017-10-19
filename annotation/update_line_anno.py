dc08f1fa750319757f3bc63f6fb17248a35e8604
henryjuho <juhob@hotmail.co.uk>
2017-10-19 12:53:36 +0100

    anno update script

---
+++ annotation/update_line_anno.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import sys
+
+
+"""
+converts variant annotations in GT LINE vcf to format to work with drosophila scripts
+"""
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if line.startswith('#'):
+        print(line)
+    elif 'ANNO=intergenic' in line:
+        print(line.replace('ANNO=intergenic', 'ANNO=intergenic_ar'))
+    elif 'ANNO=intron' in line:
+        print(line.replace('ANNO=intron_ar', 'ANNO=intron_ar'))
+    else:
+        print(line)
