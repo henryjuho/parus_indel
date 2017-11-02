391cd900da389d1f1493dd228c3d271276c9651d
henryjuho <juhob@hotmail.co.uk>
2017-11-02 10:19:33 +0000

    div plot script

---
+++ summary_analyses/collate_indel_divergence.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(dplyr)
+
+cds_div = read.delim('/Users/henryjuho/sharc_fastdata/GT_data/BGI_BWA_GATK/Summary_stats/gt_cds_indel_divergence.txt')
+non_coding_div = read.delim('/Users/henryjuho/sharc_fastdata/GT_data/BGI_BWA_GATK/Summary_stats/gt_noncoding_indel_divergence.txt')
+
+cds_div$type = 'cds'
+non_coding_div$type = 'non-coding'
+
+div = rbind(cds_div, non_coding_div)
+
+all_chr = summarise(group_by(subset(div, chromo != 'X' & chromo != 'XHet' & chromo != 'YHet' & chromo != 'chrZ'), type),
+    chromo='autosomes', indels=sum(indels), callable=sum(callable))
+
+all_chr$divergence = all_chr$indels / all_chr$callable
+
+div_plot = ggplot(all_chr, aes(x=type, y=divergence)) +
+    geom_bar(stat='identity', position='dodge') +
+    theme_bw(base_size = 10) +
+    xlab('') + ylab('divergence')
+
+pdf(file='indel_divergence.pdf', width=3, height=3)
+div_plot
+dev.off()
+
+png(file='indel_divergence.png', width=3, height=3, units='in', res=320)
+div_plot
+dev.off()
+
+out_csv = rbind(div, all_chr)
+write.csv(out_csv, file='indel_divergence.csv', row.names=FALSE)
