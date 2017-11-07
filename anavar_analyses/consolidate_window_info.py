57022a965ce05ea26fbede45d0a8c18c527e283f
henryjuho <juhob@hotmail.co.uk>
2017-11-07 12:38:47 +0000

    added theta pi and tajd per window

--- anavar_analyses/consolidate_window_info.py
+++ anavar_analyses/consolidate_window_info.py
@@ -3,6 +3,11 @@
 from __future__ import print_function
 from vcf2raw_sfs import vcf2sfs
 import gzip
+from window_sel_vs_neu_anavar import window_call_sites
+import pysam
+import sys
+sys.path.insert(0, '/Users/henryjuho/parus_indel/summary_analyses')
+from summary_stats_gt import theta_w, pi, tajimas_d
 
 
 def rec_rates(rec_file):
@@ -20,6 +25,7 @@ def rec_rates(rec_file):
 
 def main():
 
+    # paths
     vcf = '/Users/henryjuho/sharc_fastdata/GT_data/BGI_BWA_GATK/Analysis_ready_data/' \
           'final/bgi_10birds.filtered_indels.pol.anno.recomb.line.vcf.gz'
 
@@ -29,34 +35,54 @@ def main():
     windows = '/Users/henryjuho/sharc_fastdata/GT_data/BGI_BWA_GATK/anavar_analysis/' \
               'window_analysis/gt_windows.2Mb.bed.gz'
 
-    rec_data = rec_rates(rec)
+    call_sites = pysam.FastaFile('/Users/henryjuho/sharc_fastdata/GT_data/BGI_BWA_GATK/'
+                                 'Callable_sites/bgi_10birds.callable.fa')
 
-    print('chr', 'start', 'stop', 'window', 'rec_rate', 'n_indels', 'n_ins', 'n_del', sep='\t')
+    noncoding_bed = '/Users/henryjuho/sharc_fastdata/GT_ref/gt_noncoding.bed.gz'
+    rec_data = rec_rates(rec)
+    nc_bed = pysam.TabixFile(noncoding_bed)
+
+    # header
+    print('chr', 'start', 'stop', 'window', 'rec_rate', 'callable',
+          'n_ins', 'theta_ins', 'pi_ins', 'tajd_ins',
+          'n_del', 'theta_del', 'pi_del', 'tajd_del', sep='\t')
 
     for line in gzip.open(windows):
 
-        chromo, start, stop, id = line.split()
+        chromo, start, stop, wind_id = line.split()
 
-        # no variants
-        indels = vcf2sfs(vcf, mode='indel', fold=True,
-                         chromo=chromo, start=int(start), stop=int(stop),
-                         regions=['intron', 'intergenic'])
-        no_indels = len(list(indels))
-
+        # sfs
         ins = vcf2sfs(vcf, mode='ins',
                       chromo=chromo, start=int(start), stop=int(stop),
                       regions=['intron', 'intergenic'])
-        no_ins = len(list(ins))
+        ins = list(ins)
 
         dels = vcf2sfs(vcf, mode='del',
                        chromo=chromo, start=int(start), stop=int(stop),
                        regions=['intron', 'intergenic'])
-        no_dels = len(list(dels))
+        dels = list(dels)
+
+        # callsites
+        n_call = window_call_sites(call_sites, nc_bed, (chromo, int(start), int(stop)))
+
+        if len(ins) == 0 or len(dels) == 0 or n_call == 0:
+            ins_t, ins_pi, ins_taj, dels_t, dels_pi, dels_taj = 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'
+        else:
+            # summary stats
+            ins_t = theta_w(20, len(ins)) / float(n_call)
+            ins_pi = pi(20, ins) / float(n_call)
+            ins_taj = tajimas_d(20, ins)
+
+            dels_t = theta_w(20, len(dels)) / float(n_call)
+            dels_pi = pi(20, dels) / float(n_call)
+            dels_taj = tajimas_d(20, dels)
 
         # rec rate
-        rr = rec_data[id]
+        rr = rec_data[wind_id]
 
-        print(chromo, start, stop, id, rr, no_indels, no_ins, no_dels, sep='\t')
+        print(chromo, start, stop, wind_id, rr, n_call,
+              len(ins), ins_t, ins_pi, ins_taj,
+              len(dels), dels_t, dels_pi, dels_taj, sep='\t')
 
 
 if __name__ == '__main__':
