6946926a20a6afa8804e78b4d16c9875f537c134
henryjuho <juhob@hotmail.co.uk>
2017-12-01 13:06:52 +0000

    changed x to distance

--- gene_proximity_analyses/proximity_plots.R
+++ gene_proximity_analyses/proximity_plots.R
@@ -9,38 +9,44 @@ library(gtools)
 
 cbPalette <- c("#E69F00", 'tomato 3', 'steel blue', "#999999", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-prox_data = read.csv('gt_sel_neu_ref_genedist.results.csv')
+prox_data = subset(read.csv('gt_sel_neu_ref_genedist.results.csv'), bin!='10-16')
 
 prox_data$bin = factor(prox_data$bin, levels=mixedsort(as.character(subset(prox_data, sel_type=='sel' & var_type=='ins')$bin)))
 prox_data$mean_gamma = as.numeric(as.character(prox_data$shape)) * as.numeric(as.character(prox_data$scale)) * -1
+prox_data$distance = as.numeric(prox_data$bin) * 50000
 
-ins_theta = subset(prox_data, sel_type == 'sel' & var_type == 'ins', select=c(bin, theta))
-colnames(ins_theta) = c('bin', 'ins_theta')
+ins_theta = subset(prox_data, sel_type == 'sel' & var_type == 'ins', select=c(bin, theta, distance))
+colnames(ins_theta) = c('bin', 'ins_theta', 'distance')
 
-del_theta = subset(prox_data, sel_type == 'sel' & var_type == 'del', select=c(bin, theta))
-colnames(del_theta) = c('bin', 'del_theta')
+del_theta = subset(prox_data, sel_type == 'sel' & var_type == 'del', select=c(bin, theta, distance))
+colnames(del_theta) = c('bin', 'del_theta', 'distance')
 
 rdi_data = as.data.frame(cbind(ins_theta, del_theta$del_theta))
 rdi_data$rdi = rdi_data$del_theta / rdi_data$ins_theta
 
-theta_plot = ggplot(subset(prox_data, sel_type=='sel'), aes(x=bin, y=theta, colour=var_type)) +
+# tests
+cor.test(as.numeric(ins_theta$ins_theta), ins_theta$distance, method='spearman', exact=NULL)
+cor.test(as.numeric(del_theta$del_theta), del_theta$distance, method='spearman', exact=NULL)
+cor.test(as.numeric(rdi_data$rdi), rdi_data$distance, method='spearman', exact=NULL)
+
+theta_plot = ggplot(subset(prox_data, sel_type=='sel'), aes(x=distance, y=theta, colour=var_type)) +
+      geom_point(stat='identity', size = 2) +
+      theme_bw() + geom_smooth(method='lm') +
+      scale_colour_manual(values=cbPalette) +
+      ylab(expression(theta))  + xlab('Distance from gene (bp)') +
+      theme(legend.title = element_blank(), legend.position = c(0.8, 0.6), legend.background = element_blank())
+
+gamma_plot = ggplot(subset(prox_data, sel_type=='sel'), aes(x=distance, y=mean_gamma, colour=var_type)) +
       geom_point(stat='identity', size = 2) +
       theme_bw() +
       scale_colour_manual(values=cbPalette) +
-      ylab(expression(theta))  + xlab('Distance from gene / 50kb') +
-      theme(legend.title = element_blank(), legend.position = c(0.8, 0.5), legend.background = element_blank())
+      ylab(expression(gamma))  + xlab('Distance from gene (bp)') +
+      theme(legend.title = element_blank(), legend.position = 'none', legend.background = element_blank())
 
-gamma_plot = ggplot(subset(prox_data, sel_type=='sel'), aes(x=bin, y=mean_gamma, colour=var_type)) +
+rdi_plot = ggplot(rdi_data, aes(x=distance, y=rdi)) +
       geom_point(stat='identity', size = 2) +
       theme_bw() +
-      scale_colour_manual(values=cbPalette) +
-      ylab(expression(gamma))  + xlab('Distance from gene / 50kb') +
-      theme(legend.title = element_blank(), legend.position = c(0.8, 0.5), legend.background = element_blank())
-
-rdi_plot = ggplot(rdi_data, aes(x=bin, y=rdi)) +
-      geom_point(stat='identity', size = 2) +
-      theme_bw() +
-      ylab('rDI')  + xlab('Distance from gene / 50kb')
+      ylab('rDI')  + xlab('Distance from gene (bp)')
 
 png(file='distance_estimates.png', width=9, height=3, units='in', res=360)
 
