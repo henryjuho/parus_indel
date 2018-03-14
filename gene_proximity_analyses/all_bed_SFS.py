d8282d40e885ec4d896fef5d9515949123e35169
henryjuho <juhob@hotmail.co.uk>
2018-03-14 15:59:50 +0000

    path update

--- gene_proximity_analyses/all_bed_SFS.py
+++ gene_proximity_analyses/all_bed_SFS.py
@@ -22,7 +22,7 @@ def main():
         os.makedirs(args.out_dir)
 
     # sfs bed append cmd
-    cmd = 'zgrep ^{chromo} {bed} | python addSFS.py {vcf} {call_fa} > {output}'
+    cmd = 'zgrep ^{chromo} {bed} | python ~/parus_indel/gene_proximity_analyses/addSFS.py {vcf} {call_fa} > {output}'
 
     # input bed files
     bed_list = [args.bed_dir + x for x in os.listdir(args.bed_dir) if x.endswith('.bed.gz')]
