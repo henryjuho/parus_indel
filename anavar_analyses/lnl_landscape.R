e62cb1538f3dfb5c3b722989950cd37299691993
henryjuho <juhob@hotmail.co.uk>
2018-07-02 15:52:12 +0100

    lnl plotting

---
+++ anavar_analyses/lnl_landscape.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+
+res = read.delim('all_models_lnl.txt')
+
+png('all_mods_lnl.png', width=12, height=8, units='in', res=320)
+ggplot(res, aes(x=rank, y=lnL)) +
+    geom_line(stat='identity') +
+    facet_wrap(~model, nrow=4, ncol=4, scales = "free") +
+    xlab('searches')
+
+dev.off()
