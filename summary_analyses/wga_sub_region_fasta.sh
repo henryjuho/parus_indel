1e19e8601019ce2b73dd4ccf8064020a0f1aa372
henryjuho <juhob@hotmail.co.uk>
2018-07-27 17:21:59 +0100

    rectified ar path

--- summary_analyses/wga_sub_region_fasta.sh
+++ summary_analyses/wga_sub_region_fasta.sh
@@ -21,7 +21,7 @@ cd /fastdata/bop15hjb/hjb/GT_data/BGI_BWA_GATK/snp_divergence/
 zcat gt_introns.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_introns.wga &
 zcat gt_intergenic.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_intergenic.wga &
 zcat gt_cds.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_cds.wga &
-zcat /fastdata/bop15hjb/hjb/GT_ref/Greattit.Zebrafinch.Flycatcher.ancLINEs.sorted.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_ar.wga &
+zcat /fastdata/bop15hjb/hjb/GT_data/BGI_BWA_GATK/divergence_data/repeat_coordinates/LINE_intersect/Greattit.Zebrafinch.Flycatcher.LINEs.sorted.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_ar.wga &
 zcat /fastdata/bop15hjb/hjb/GT_ref/Greattit.Zebrafinch.Flycatcher.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_all.wga &
 
 wait
