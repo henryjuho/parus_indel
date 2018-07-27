d08f4a4f4dbca19da6b605d7db80547935808330
henryjuho <juhob@hotmail.co.uk>
2018-07-27 17:09:34 +0100

    fasta gen

---
+++ summary_analyses/wga_sub_region_fasta.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+#$-l h_rt=72:00:00
+#$-l mem=6G
+#$-l rmem=2G
+
+#$-pe openmp 5
+#$-P evolgen
+#$-q evolgen.q
+
+#$-N wga_subregions2fa.sh
+#$-o /fastdata/bop15hjb/hjb/GT_data/BGI_BWA_GATK/snp_divergence/wga_subregions2fa.out
+#$-e /fastdata/bop15hjb/hjb/GT_data/BGI_BWA_GATK/snp_divergence/wga_subregions2fa.error
+
+#$-V
+
+cd /fastdata/bop15hjb/hjb/GT_data/BGI_BWA_GATK/snp_divergence/
+
+zcat gt_introns.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_introns.wga &
+zcat gt_intergenic.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_intergenic.wga &
+zcat gt_cds.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_cds.wga &
+zcat /fastdata/bop15hjb/hjb/GT_ref/Greattit.Zebrafinch.Flycatcher.ancLINEs.sorted.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_ar.wga &
+zcat /fastdata/bop15hjb/hjb/GT_ref/Greattit.Zebrafinch.Flycatcher.wga.bed.gz | ~/parus_indel/summary_analyses/wga2fa.py -out_stem gt_all.wga &
+
+wait
+
+
