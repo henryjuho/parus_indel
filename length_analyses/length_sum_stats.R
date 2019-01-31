85f6d81be715aeb9b480737cf397def3850fb0e6
henryjuho <juhob@hotmail.co.uk>
2019-01-31 17:21:08 +0000

    sorted n=20 bug

--- length_analyses/length_sum_stats.R
+++ length_analyses/length_sum_stats.R
@@ -27,7 +27,13 @@ ncins_cor = cor.test(nc_ins$tajd, nc_ins$length, method='spearman')
 nc_del = subset(all_dat, region=='Non-coding' & var_type=='del')
 ncdel_cor = cor.test(nc_del$tajd, nc_del$length, method='spearman')
 
-taj_plot = ggplot(all_dat, aes(x=length, y=tajd, colour=var_type)) +
+avg_dat = data.frame(tajd=c(-0.972881511536, -1.26992351684, -0.325084323868, -0.463188762236),
+                     var_type=c('ins', 'del', 'ins', 'del'),
+                     region=c('CDS', 'CDS', 'Non-coding', 'Non-coding'))
+
+
+taj_plot = ggplot(all_dat, aes(x=length, y=tajd, colour=var_type, fill=var_type)) +
+    geom_hline(data = avg_dat, aes(yintercept=tajd, colour=var_type)) +
     geom_point(stat='identity') +
     theme_bw(base_size=10) +
     scale_colour_manual(values=viridis(3)) +
@@ -36,12 +42,27 @@ taj_plot = ggplot(all_dat, aes(x=length, y=tajd, colour=var_type)) +
     theme(legend.title=element_blank(), legend.position=c(0.9, 0.8),
     plot.title = element_text(size=10, colour='steel blue', face='bold')) +
     ggtitle(paste("non coding insertions: Spearman's", expression(rho), "=", signif(ncins_cor$estimate, 3),
-           'p =', signif(ncins_cor$p.value, 3),
+           'p =', signif(ncins_cor$p.value, 6),
            "\nnon coding deletions: Spearman's", expression(rho), "=", signif(ncdel_cor$estimate, 3),
            'p =', signif(ncdel_cor$p.value, 3),sep=' '))
 
 png('indel_length_summary_stats.png', width=6, height=6, res=320, units='in')
 
 grid.arrange(pi_plot, taj_plot, ncol=1)
+
+dev.off()
+
+# supp mat plot
+
+pdf('indel_length_tajd.pdf', width=6, height=3)
+
+ggplot(all_dat, aes(x=length, y=tajd, colour=var_type, fill=var_type)) +
+    geom_hline(data = avg_dat, aes(yintercept=tajd, colour=var_type)) +
+    geom_point(stat='identity') +
+    theme_bw(base_size=10) +
+    scale_colour_manual(values=viridis(3)) +
+    facet_wrap(~region, nrow=1) +
+    labs(x='INDEL length (bp)', y="Tajima's D")+
+    theme(legend.title=element_blank(), legend.position=c(0.4, 0.8), legend.background=element_blank())
 
 dev.off()
