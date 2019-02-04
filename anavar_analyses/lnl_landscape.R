0ed071d76fc786483906d97dba1d63669c70996f
henryjuho <juhob@hotmail.co.uk>
2019-02-04 11:24:43 +0000

    new mods

--- anavar_analyses/lnl_landscape.R
+++ anavar_analyses/lnl_landscape.R
@@ -2,10 +2,10 @@ library(ggplot2)
 
 res = read.delim('all_models_lnl.txt')
 
-png('all_mods_lnl.png', width=12, height=8, units='in', res=320)
+png('all_mods_lnl.png', width=12, height=12, units='in', res=320)
 ggplot(res, aes(x=rank, y=lnL)) +
     geom_line(stat='identity') +
-    facet_wrap(~model, nrow=4, ncol=4, scales = "free") +
+    facet_wrap(~model, ncol=4, scales = "free") +
     xlab('searches')
 
 dev.off()
