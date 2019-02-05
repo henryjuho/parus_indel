13d12a69fd4f7b4bd7989ffb767915d12b41a10d
henryjuho <juhob@hotmail.co.uk>
2019-02-05 19:15:09 +0000

    with frameshift and pol correct

--- summary_analyses/summary_stats.R
+++ summary_analyses/summary_stats.R
@@ -1,89 +1,87 @@
 library(ggplot2)
 library(gridExtra)
 library(dplyr)
+library(viridis)
 
 summary_data = read.delim('bgi10_stats.txt', na=0)
+summary_data_sfs_correct = read.delim('bgi10_stats_sfs_corrected.txt', na=0)
+
+summary_data$sfs = 'raw'
+summary_data_sfs_correct$sfs = 'corrected'
+
+all_sum = rbind(summary_data, summary_data_sfs_correct)
+
 call_data = read.delim('bgi10_call.txt')
 
-all_data = cbind(summary_data, call_data)
-
-all_data[7] = NULL
-all_data[7] = NULL
+all_data = dplyr::left_join(all_sum, call_data, by=c('category', 'variation'))
 
 all_data$tw_per_site = all_data$theta_w / all_data$call
 all_data$pi_per_site = all_data$pi / all_data$call
 
+str(all_data)
+
 write.csv(all_data, 'bgi10_summary_stats.csv', row.names=FALSE)
 
 all_data = subset(all_data, category != 'noncoding_noUCNEs'
-  & category != 'cds_frameshift' & category != 'cds_non_frameshift'
+  & category != 'UCNE' & category != 'ALL'
   & category != 'noncoding')
+#
+# all_data$category = factor(all_data$category,
+#   levels=rev(c('cds_frameshift', 'cds_non_frameshift', 'CDS', '0fold', 'nonsense', '4fold', 'introns', 'intergenic', 'AR')))
+#
 
-all_data$category = factor(all_data$category,
-  levels=rev(c('CDS', '0fold', 'nonsense', 'UCNE', '4fold', 'ALL', 'introns', 'intergenic', 'AR')))
+indel_data = subset(all_data, variation!='SNP' & category!='0fold' & category!='4fold' & category!='nonsense')
+snp_data = subset(all_data, variation=='SNP' & category!='cds_frameshift' & category!='cds_non_frameshift')
+str(snp_data)
+filtered_data = rbind(indel_data, snp_data)
 
-# reset 0fold 4fold nonsense indel tajd to 0
-
-# 0fold
-all_data[2,]$tajD = NA
-all_data[3,]$tajD = NA
-all_data[4,]$tajD = NA
-all_data[2,]$pi_per_site = NA
-all_data[3,]$pi_per_site = NA
-all_data[4,]$pi_per_site = NA
-all_data[2,]$tw_per_site = NA
-all_data[3,]$tw_per_site = NA
-all_data[4,]$tw_per_site = NA
-
-# 4fold
-all_data[6,]$tajD = NA
-all_data[7,]$tajD = NA
-all_data[8,]$tajD = NA
-all_data[6,]$pi_per_site = NA
-all_data[7,]$pi_per_site = NA
-all_data[8,]$pi_per_site = NA
-all_data[6,]$tw_per_site = NA
-all_data[7,]$tw_per_site = NA
-all_data[8,]$tw_per_site = NA
+indel_data$category = factor(indel_data$category, levels=c('cds_frameshift', 'cds_non_frameshift', 'CDS',
+                                                           'introns', 'intergenic', 'AR'))
 
 # theta_w
-tw = ggplot(all_data, aes(x=category, y=tw_per_site, colour=variation))+
-  geom_point(stat='identity', position = position_dodge(width=0.9), size = 2) +
+tw = ggplot(indel_data, aes(x=category, y=tw_per_site, fill=variation))+
+  geom_bar(stat='identity', position = position_dodge(width=0.9), size = 2) +
   theme_bw() +
+  facet_wrap(~sfs, ncol=1) +
   xlab('')  + ylab(expression(theta[w])) +
   theme(legend.title=element_blank(), legend.position='none',
-  axis.text.x=element_text(angle=45, hjust=1))
+  axis.text.x=element_text(angle=45, hjust=1)) +
+  scale_fill_viridis(discrete=T)
 
 # pi
-pi = ggplot(all_data, aes(x=category, y=pi_per_site, colour=variation))+
-  geom_point(stat='identity', position = position_dodge(width=0.9), size = 2) +
+pi = ggplot(indel_data, aes(x=category, y=pi_per_site, fill=variation))+
+  geom_bar(stat='identity', position = position_dodge(width=0.9), size = 2) +
   theme_bw() +
+  facet_wrap(~sfs, ncol=1) +
   xlab('')  + ylab(expression(pi)) +
   theme(legend.title=element_blank(), legend.position='none',
-  axis.text.x=element_text(angle=45, hjust=1))
+  axis.text.x=element_text(angle=45, hjust=1)) +
+  scale_fill_viridis(discrete=T)
 
 # tajD
-tajd = ggplot(all_data, aes(x=category, y=tajD, colour=variation))+
-  geom_point(stat='identity', position = position_dodge(width=0.9), size = 2) +
+tajd = ggplot(indel_data, aes(x=category, y=tajD, fill=variation))+
+  geom_bar(stat='identity', position = position_dodge(width=0.9), size = 2) +
   theme_bw() +
+  facet_wrap(~sfs, ncol=1) +
   xlab('')  + ylab("Tajima's D") +
   #ylim(-1.3, -0.2) +
-  theme(legend.title=element_blank(), legend.position=c(0.2, 0.3),
-  axis.text.x=element_text(angle=45, hjust=1))
-
-# pdf(file='gt_summary_stats.pdf', width=9, height=3)
+  theme(legend.title=element_blank(), legend.position=c(0.83, 0.65),
+  axis.text.x=element_text(angle=45, hjust=1)) +
+  scale_fill_viridis(discrete=T)
 #
-# grid.arrange(tajd, tw, pi, nrow=1)
+# # pdf(file='gt_summary_stats.pdf', width=9, height=3)
+# #
+# # grid.arrange(tajd, tw, pi, nrow=1)
+# #
+# # dev.off()
 #
-# dev.off()
-
-pdf(file='gt_tajd.pdf', width=3, height=3)
-
-tajd
-
-dev.off()
-
-png(file='gt_summary_stats.png', width=9, height=3, units='in', res=360)
+# # pdf(file='gt_tajd.pdf', width=3, height=3)
+# #
+# # tajd
+# #
+# # dev.off()
+#
+png(file='gt_summary_stats.png', width=9, height=6, units='in', res=360)
 
 grid.arrange(tajd, tw, pi, nrow=1)
 
